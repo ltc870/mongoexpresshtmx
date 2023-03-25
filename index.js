fed19a7d567a06dc90bd5e1f8345a24e370d8cf0
Lawrence C <lawrencecaudle870@gmail.com>
2023-03-25 15:16:00 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,134 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const uri = process.env.MONGO_URI;
+const Book = require("./app/model/books");
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("CONNECTION OPEN!!!");
+  })
+  .catch((err) => {
+    console.log(err, "CONNECTION FAILED!!");
+  });
+
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", async (req, res) => {
+  const books = await Book.find({});
+  return res.render("index", { books });
+});
+
+app.post("/submit", async (req, res) => {
+  const book = {
+    name: req.body.title,
+    author: req.body.author,
+  };
+  await Book.create(book).then((x) => {
+    console.log("Created Book!");
+    return res.send(`<tr>
+      <td>${req.body.title}</td>
+      <td>${req.body.author}</td>
+      <td>
+        <button class="btn btn-primary" hx-get="/get-edit-form/${x.null}">Edit Book</button>
+      </td>
+      <td>
+        <button hx-delete="/delete/${x.null}" class="btn btn-primary">Delete</button>
+      </td>
+    </tr>`);
+  });
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  await Book.findByIdAndDelete(id);
+  console.log("Deleted Book!!");
+  return res.send("");
+});
+
+app.get("/get-book-row/:id", async (req, res) => {
+  const id = req.params.id;
+  await Book.findById(id).then((book) => {
+    console.log(req.body);
+    return res.send(`<tr>
+    <td>${book.name}</td>
+    <td>${book.author}</td>
+    <td>
+      <button class="btn btn-primary" 
+        hx-get="/get-edit-form/${id}">
+        Edit Book
+      </button>
+    </td>
+    <td>
+      <button hx-delete="/delete/${id}" 
+        class="btn btn-primary">
+        Delete
+      </button>
+    </td>
+  </tr>`);
+  });
+});
+
+app.get("/get-edit-form/:id", async (req, res) => {
+  const id = req.params.id;
+  await Book.findByIdAndUpdate(id).then((book) => {
+    return res.send(`<tr hx-trigger="cancel" class="editing" hx-get="/get-book-row/${id}">
+      <td><input name="title" value="${book.name}"/></td>
+      <td><input name="author" value="${book.author}"/></td>
+      <td>
+        <button class="btn btn-primary" hx-get="/get-book-row/${id}">
+          Cancel
+        </button>
+        <button class="btn btn-primary" hx-put="/update/${id}" hx-include="closest tr">
+          Save
+        </button>
+      </td>
+    </tr>`);
+  });
+});
+
+app.put("/update/:id", async (req, res) => {
+  const id = req.params.id;
+  await Book.findByIdAndUpdate(id).then((item) => {
+    item
+      .updateOne({
+        name: req.body.title,
+        author: req.body.author,
+      })
+      .then(() => {
+        return res.send(`<tr>
+        <td>${req.body.title}</td>
+        <td>${req.body.author}</td>
+        <td>
+          <button class="btn btn-primary" 
+            hx-get="/get-edit-form/${id}">
+            Edit Book
+          </button>
+        </td>
+        <td>
+          <button hx-delete="/delete/${id}" 
+            class="btn btn-primary">
+            Delete
+          </button>
+        </td>
+      </tr>`);
+      });
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
---
+++ seeds.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const uri =
+  "mongodb+srv://caudlel:TWINpb870@cluster0.97ek6nv.mongodb.net/?retryWrites=true&w=majority";
+const Book = require("./app/model/books");
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("CONNECTION OPEN!!!");
+  })
+  .catch((err) => {
+    console.log(err, "CONNECTION FAILED!!");
+  });
+
+// const bookHarryPotter = new Book({
+//   name: "Harry Potter and the Philosopher's Stone",
+//   author: "J.K. Rowling",
+// });
+// bookHarryPotter
+//   .save()
+//   .then((bookHP) => {
+//     console.log(bookHP);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+const seedBooks = [
+  { name: "Percy Jackson and The Lightning Thief", author: "Rick Riordan" },
+  { name: "A Song of Fire and Ice", author: "George R. R. Martin" },
+  {
+    name: "The Lord of the Rings: The Fellowship of the Ring",
+    author: "J. R. R. Tolkien",
+  },
+  { name: "Animorphs", author: "K. A. Applegate" },
+];
+
+Book.insertMany(seedBooks)
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
